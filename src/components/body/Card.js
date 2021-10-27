c6997a6abda3e1bff0290192b5c0c5fc4ec221fa
유경진 <pokly0908@naver.com>
2021-10-27 18:25:47 +0900

    #4 Movie_Card

--- src/components/body/Card.js
+++ src/components/body/Card.js
@@ -4,38 +4,68 @@ class Card extends Component {
   render() {
     return (
       <div className="card_view"> 
-       <ul className="card_list">
-         <li>
-           <img src="#"/>
-           <p>제목1</p>
-           <a>예매율</a>
-           <a>평점</a>
-         </li>
-         <li>
-           <img src="#"/>
-           <p>제목2</p>
-           <a>예매율</a>
-           <a>평점</a>
-         </li>
-         <li>
-           <img src="#"/>
-           <p>제목3</p>
-           <a>예매율</a>
-           <a>평점</a>
-         </li>
-         <li>
-           <img src="#"/>
-           <p>제목4</p>
-           <a>예매율</a>
-           <a>평점</a>
-         </li>
-         <li>
-           <img src="#"/>
-           <p>제목5</p>
-           <a>예매율</a>
-           <a>평점</a>
-         </li>
-       </ul>
+        <ul className="card_list">
+          <li>
+            <div className="movie">
+              <img src="#"/>
+              <ul>
+                <li>예매하기</li>
+                <li>상세정보</li>
+              </ul>
+            </div>
+            <p>제목1</p>
+            <a>예매율</a>
+            <a>평점</a>
+          </li>
+          <li>
+            <div className="movie">
+              <img src="#"/>
+              <ul>
+                <li>예매하기</li>
+                <li>상세정보</li>
+              </ul>
+            </div>
+            <p>제목2</p>
+            <a>예매율</a>
+            <a>평점</a>
+          </li>
+          <li>
+            <div className="movie">
+              <img src="#"/>
+              <ul>
+                <li>예매하기</li>
+                <li>상세정보</li>
+              </ul>
+            </div>
+            <p>제목3</p>
+            <a>예매율</a>
+            <a>평점</a>
+          </li>
+          <li>
+            <div className="movie">
+              <img src="#"/>
+              <ul>
+                <li>예매하기</li>
+                <li>상세정보</li>
+              </ul>
+            </div>
+            <p>제목4</p>
+            <a>예매율</a>
+            <a>평점</a>
+          </li>
+          <li>
+            <div className="movie">
+              <img src="#"/>
+              <ul>
+                <li>예매하기</li>
+                <li>상세정보</li>
+              </ul>
+            </div>
+            <p>제목5</p>
+            <a>예매율</a>
+            <a>평점</a>
+          </li>
+        </ul>
       </div>
     );
   }
