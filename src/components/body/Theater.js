872da7b6ecc1230a37447239791ce084d32d766c
hyo-me <42805831+hyo-me@users.noreply.github.com>
2021-10-28 13:11:56 +0900

    Add files via upload

---
+++ src/components/body/Theater.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+class Theater extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className = "Theater">영화관</h1>
+        <div className = "Show">
+            <img src = "#"/>
+            <img src = "#"/>
+            <img src = "#"/>
+            <img src = "#"/>
+            <img src = "#"/>
+            <img src = "#"/>
+            <img src = "#"/>
+            <img src = "#"/>
+            <img src="#" />
+            <img src="#" />
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default Theater;
