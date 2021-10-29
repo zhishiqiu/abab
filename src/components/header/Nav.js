37ea345851a181758f76d8fbea29f8d449b6d9d4
hyo-me <42805831+hyo-me@users.noreply.github.com>
2021-10-29 12:29:46 +0900

    Update Nav.js

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -7,18 +7,18 @@ class Nav extends Component{
           <ul className="Nav_bar">
             <li className="Nav"><a href="#">영화</a>
               <ul className="Sub_bar">
-                <li className="Sub"><a href="#">영화차트</a></li>
-                <li className="Sub"><a href="#">뭐...등등등</a></li>
+                <li className="Sub"><a href="#">전체 영화</a></li>
+                <li className="Sub"><a href="#">etc</a></li>
               </ul>
             </li>
             <li className="Nav"><a href="#">예매</a>
             <ul className="Sub_bar">
-                <li className="Sub"><a href="#">시간표</a></li>
-                <li className="Sub"><a href="#">상영관</a></li>
+                <li className="Sub"><a href="#">시간대 별</a></li>
+                <li className="Sub"><a href="#">상영관 별</a></li>
               </ul></li>
-            <li className="Nav"><a href="#">극장</a>
+            <li className="Nav"><a href="#">상영관</a>
             <ul className="Sub_bar">
-                <li className="Sub"><a href="#">극장</a></li>
+                <li className="Sub"><a href="#">전체</a></li>
                 <li className="Sub"><a href="#">특별관</a></li>
               </ul></li>
             <li className="Nav"><a href="#">이벤트</a>
