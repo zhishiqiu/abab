86ff08e3294c8180a22b1f683b63c460490b0dc0
halikes <54992344+halikes@users.noreply.github.com>
2021-10-30 13:31:01 +0900

    Create Event.js
    create event.js

---
+++ src/components/Event.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Event extends Component {
+    render() {
+        return (
+            <div>
+                <h1 class="Event">Event</h1>
+                <div class="Event_box">
+                    <div class="Event_box1">
+                        <img src = "#" class="event1"></img>
+                    </div>
+                    <div class="Event_box2">
+                        <img src = "#" class="event2"></img>
+                        <img src = "#" class="event3"></img>
+                    </div>
+                    <div class="Event_box3">
+                        <img src = "#" class="event4"></img>
+                        <img src = "#" class="event5"></img>
+                    </div>
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Event;
